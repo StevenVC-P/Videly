d5c800f57c1f42651e58d63809f9455f1524f13a
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2021-12-15 10:13:47 -0600

    added pagnation, selctor rendering correctetly

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart as faHeartRegular} from '@fortawesome/free-regular-svg-icons'
 import { faHeart as faHeartSolid} from '@fortawesome/free-solid-svg-icons'
 
-class Like extends Component {
-    render(){ 
-        let classes = {}
-        if (!this.props.liked) classes = faHeartRegular
-        else classes = faHeartSolid
-        return(
-            <FontAwesomeIcon 
-                onClick={this.props.onClick} 
-                style={{cursor: 'pointer'}} 
-                icon={classes}
-            />
-        )
-    }
-
-}
+const Like = props => {
+    let classes = {}
+    if (!props.liked) classes = faHeartRegular
+    else classes = faHeartSolid
+    return(
+        <FontAwesomeIcon 
+            onClick={props.onClick} 
+            style={{cursor: 'pointer'}} 
+            icon={classes}
+        />
+    );
+};
  
 export default Like;
